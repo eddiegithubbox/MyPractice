415f212da9b6637d0111f00195afee6dc1e2e0e6
Eddie <eddie@2001-b400-e330-cd9e-e029-c204-c58e-89f5.emome-ip6.hinet.net>
2018-05-16 11:14:41 +0800

    first slave commit

--- MyPractice/Controller/ViewController.swift
+++ MyPractice/Controller/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("I am Slave")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
